19942d5d4a52ab391cfd93cebd517fdba6122913
Luis Martínez <lifm9816@gmail.com>
2023-09-16 23:06:18 -0600

    Agregación de rutas y creación de página '404'

--- src/App.js
+++ src/App.js
@@ -2,28 +2,19 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home.jsx"
 import Sobre from "./pages/Sobre.jsx"
+import Page404 from "./pages/Page404.jsx"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-
-  const location = window.location.pathname;
-  console.log(location);
-
-  const Router = () => 
-  {
-    if(location === "/")
-    {
-      return <Home />
-    }
-    else
-    {
-      return <Sobre />
-    }
-  }
-
+  
   return (
-    <>
-      {Router()}
-    </>
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Home/>} />
+          <Route path = "/sobre" element = {<Sobre/>} />
+          <Route path = "*" element = {<Page404/>} />
+        </Routes>
+      </Router>
   );
 }
 
