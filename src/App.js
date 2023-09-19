a7f124996e1e6c038a78482bb06ac7e0e3c369a2
Luis Martínez <lifm9816@gmail.com>
2023-09-18 19:20:45 -0600

    Creación de api para lectura de articulos mediante axios, uso de 'Link' para cambiar de vista (función de react-router-dom) e implementación de header y navbar de la página

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './assets/css/componentes/card.css'
 import Home from "./pages/Home.jsx"
 import Sobre from "./pages/Sobre.jsx"
 import Page404 from "./pages/Page404.jsx"
+import Header from "./components/Header.jsx"
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   
   return (
       <Router>
+        <Header />
         <Routes>
           <Route path = "/" element = {<Home/>} />
           <Route path = "/sobre" element = {<Sobre/>} />
